993341fe6acb84b924b7f683abac920b2c4d62c3
ddm50 <53798547+ddm50@users.noreply.github.com>
2019-08-31 11:49:46 +0200

    Update EbayASMUND.py

--- EbayASMUND.py
+++ EbayASMUND.py
@@ -1,12 +1,11 @@
 from ebaysdk.trading import Connection
 import json, xmltodict
 from collections import OrderedDict
-# I will release this in the future as a module once I make sure every important function is included and it's stable.
 
 class TheEbayAsmund:
     def connect_and_execute(self):
         ''''''
-        self.api = Connection(config_file='ebay.yaml', domain='api.sandbox.ebay.com', debug=False)
+        self.api = Connection(config_file='ebay.yaml', domain='api.ebay.com', debug=False)
     def xmltojson(self, function):
         self.output_dict = json.loads(json.dumps(xmltodict.parse(function)))
     def list(self):
@@ -22,7 +21,7 @@ class TheEbayAsmund:
         self.PayPal_EMAIL = self.item_info['PayPal_EMAIL']
         self.description = self.item_info['Description']
         self.return_description = self.item_info['ReturnDescription']
-        self.handling_time = self.item_info['Description']
+        self.handling_time = self.item_info['handling_time']
         self.Price = self.item_info['Price']
 
         self.request = {
@@ -37,7 +36,7 @@ class TheEbayAsmund:
                 "PrimaryCategory": {"CategoryID": "33963"},
                 "Description": self.description,
                 "ListingDuration": "GTC",
-                "StartPrice": self.price,
+                "StartPrice": self.Price,
                 "Currency": "USD",
                 "ReturnPolicy": {
                     "ReturnsAcceptedOption": "ReturnsAccepted",
@@ -62,16 +61,31 @@ class TheEbayAsmund:
         self.seller_transactions = self.api.response_content()
         self.xmltojson(function=self.seller_transactions)
         #["Buyer"]["Email"]
-        self.seller_id = self.output_dict['GetSellerTransactionsResponse']['Seller']['UserID']
-        self.buyer_mail = self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['Buyer']['Email']
-        self.buyer_feedback = self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['Buyer']['FeedbackScore']
-        self.buyer_id = self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['Buyer']['UserID']
-        self.buyer_country = self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['Buyer']['BuyerInfo']['ShippingAddress']['CountryName']
-        self.item_id = self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['Item']['ItemID']
-        self.transaction_id = self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['TransactionID']
-        self.quantity = self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['Item']['Quantity']
-        self.price = self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['Item']['SellingStatus']['CurrentPrice']['#text']
-        print(f"Buyer ID: {self.buyer_id} | Buyer Email: {self.buyer_mail} | Buyer Feedback: {self.buyer_feedback} | Country: {self.buyer_country} | Item ID: {self.item_id} | Quantity: {self.quantity} | Transaction ID: {self.transaction_id} | Price: $%s" % self.price)
+        if int(self.output_dict["GetSellerTransactionsResponse"]["ReturnedTransactionCountActual"]) > 1:
+            self.seller_id = self.output_dict['GetSellerTransactionsResponse']['Seller']['UserID']
+            self.buyer_mail = \
+            self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['Buyer']['Email']
+            self.buyer_feedback = \
+            self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['Buyer'][
+                'FeedbackScore']
+            self.buyer_id = \
+            self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['Buyer']['UserID']
+            self.buyer_country = \
+            self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['Buyer'][
+                'BuyerInfo']['ShippingAddress']['CountryName']
+            self.item_id = \
+            self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['Item']['ItemID']
+            self.transaction_id = \
+            self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['TransactionID']
+            self.quantity = \
+            self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['Item']['Quantity']
+            self.price = \
+            self.output_dict["GetSellerTransactionsResponse"]['TransactionArray']['Transaction'][0]['Item'][
+                'SellingStatus']['CurrentPrice']['#text']
+            print(
+                f"Buyer ID: {self.buyer_id} | Buyer Email: {self.buyer_mail} | Buyer Feedback: {self.buyer_feedback} | Country: {self.buyer_country} | Item ID: {self.item_id} | Quantity: {self.quantity} | Transaction ID: {self.transaction_id} | Price: $%s" % self.price)
+        else:
+            print("EBAY: Sorry, but we found no transactions.")
     def send_message(self):
         try:
             self.gather_info()
@@ -93,14 +107,18 @@ class TheEbayAsmund:
             print("EBAY RESPONSE: Marked shipment as sent.")
     def getaccount(self):
         '''
-        GETS DATA FROM THE SELLER DASHBOARD SUCH AS POWER STATUS LEVEL WILL ADD MORE SOON
+        GETS DATA FROM THE SELLER DASHBOARD SUCH AS LEVEL
         :return:
         '''
         self.gather_info()
-        self.api.execute('GetSellerDashboard')
-        self.xmltojson(self.api.response_content())
-        self.SellerDashboard = self.output_dict["GetSellerDashboardResponse"]
-        if self.output_dict["GetSellerDashboardResponse"]["Ack"] == "Success":
-            print(f"User ID: {self.seller_id} | Power Level Status: {self.SellerDashboard['PowerSellerStatus']['Level']}")
+        try:
+            self.api.execute('GetSellerDashboard')
+            self.xmltojson(self.api.response_content())
+            self.SellerDashboard = self.output_dict["GetSellerDashboardResponse"]
+            if self.output_dict["GetSellerDashboardResponse"]["Ack"] == "Success":
+                print(
+                        f"User ID: {self.seller_id} | Power Level Status: {self.SellerDashboard['PowerSellerStatus']['Level']}")
+        except:
+            print("EBAY ERROR: Either an error occured or you don't have access to seller dashboard which means you haven't opened an seller account.")
 Ebay = TheEbayAsmund()
-Ebay.getaccount()
+Ebay.list()
